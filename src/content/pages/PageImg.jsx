7fc55101f51e7c805c23b0d1ec3af680a089378c
oknalv <eblebrero@gmail.com>
2024-11-26 19:44:03 +0100

    homepage test

--- src/content/pages/PageImg.jsx
+++ src/content/pages/PageImg.jsx
@@ -4,11 +4,10 @@ import "./PageImg.css";
 function PageImg({fontSize, size, src}) {
     const imgHeight = fontSize[1] * size;
     const imgPadding = fontSize[0] - (imgHeight % fontSize[0]);
-    const imageUrl = new URL(`.${src}`, import.meta.url).href;
     return(
         <img
             className="page-img"
-            src={imageUrl}
+            src={src}
             style={{
                 padding: `0 ${imgPadding / 2}px`,
                 height: `${imgHeight}px`
