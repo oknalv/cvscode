db2ffc541613ff183b55f59a7992bce9b5918abf
oknalv <eblebrero@gmail.com>
2024-11-26 19:20:38 +0100

    Image url fix

--- src/content/pages/PageImg.jsx
+++ src/content/pages/PageImg.jsx
@@ -4,10 +4,11 @@ import "./PageImg.css";
 function PageImg({fontSize, size, src}) {
     const imgHeight = fontSize[1] * size;
     const imgPadding = fontSize[0] - (imgHeight % fontSize[0]);
+    const imageUrl = new URL(src, import.meta.url).href;
     return(
         <img
             className="page-img"
-            src={src}
+            src={imageUrl}
             style={{
                 padding: `0 ${imgPadding / 2}px`,
                 height: `${imgHeight}px`
